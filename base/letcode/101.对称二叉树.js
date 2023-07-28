564bc557322d4711bf74bb644996aa4985d30773
lucklyrs <1305029103@qq.com>
2023-07-28 17:13:27 +0800

    fix: stack  isSymmetric

--- base/letcode/101.对称二叉树.js
+++ base/letcode/101.对称二叉树.js
@@ -41,21 +41,53 @@
 // }
 
 // 通过队列实现判断对称二叉树
+// var isSymmetric = function (root) {
+//   if (root === null) {
+//     return true
+//   }
+//   let queue = [] //队列
+//   queue.push(root.left)
+//   queue.push(root.right)
+//   while (queue.length) {
+//     let leftNode = queue.shift()
+//     let rightNode = queue.shift()
+//     // 左右节点都为空时
+//     if (leftNode === null && rightNode === null) {
+//       continue
+//     }
+//     //左右节点有不为空，或者都不为空且值不同时
+//     if (
+//       leftNode === null ||
+//       rightNode === null ||
+//       leftNode.val !== rightNode.val
+//     ) {
+//       return false
+//     }
+//     //队列先进先出
+//     queue.push(leftNode.left)
+//     queue.push(rightNode.right)
+//     queue.push(leftNode.right)
+//     queue.push(rightNode.left)
+//   }
+//   return true
+// }
+
+// 通过栈的实现迭代是否为对称二叉树
 var isSymmetric = function (root) {
+  // 栈 先进后出
   if (root === null) {
     return true
   }
-  let queue = [] //队列
-  queue.push(root.left)
-  queue.push(root.right)
-  while (queue.length) {
-    let leftNode = queue.shift()
-    let rightNode = queue.shift()
-    // 左右节点都为空时
+  let stack = []
+  stack.push(root.left)
+  stack.push(root.right)
+  while (stack.length) {
+    let leftNode = stack.pop()
+    let rightNode = stack.pop()
+    // 左节点与右节点
     if (leftNode === null && rightNode === null) {
       continue
     }
-    //左右节点有不为空，或者都不为空且值不同时
     if (
       leftNode === null ||
       rightNode === null ||
@@ -63,11 +95,10 @@ var isSymmetric = function (root) {
     ) {
       return false
     }
-    //队列先进先出
-    queue.push(leftNode.left)
-    queue.push(rightNode.right)
-    queue.push(leftNode.right)
-    queue.push(rightNode.left)
+    stack.push(leftNode.left)
+    stack.push(rightNode.right)
+    stack.push(leftNode.right)
+    stack.push(rightNode.left)
   }
   return true
 }
