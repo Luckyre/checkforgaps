9bccbd5433299671a64fc33e38e4cd6e525eb8dc
lucklyrs <1305029103@qq.com>
2023-08-01 13:57:46 +0800

    feat: generate

---
+++ base/letcode/118.杨辉三角.js
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=118 lang=javascript
+ *
+ * [118] 杨辉三角
+ */
+
+// @lc code=start
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function (numRows) {
+  //4
+  const res = []
+  for (let i = 0; i < numRows; i++) {
+    const row = new Array(i + 1).fill(1) // [1], [1,1], [1,1,1]
+    for (let j = 1; j < row.length - 1; j++) {
+      //
+      row[j] = res[i - 1][j - 1] + res[i - 1][j]
+    }
+    res.push(row) //[[1],[1,1]]
+  }
+  return res
+}
+// @lc code=end
