39758d993e2ece355c3fc6c1ac30b36eb1c893ea
lucklyrs <1305029103@qq.com>
2023-08-02 18:18:11 +0800

    feat: maxProfit

---
+++ base/letcode/121.买卖股票的最佳时机.js
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=121 lang=javascript
+ *
+ * [121] 买卖股票的最佳时机
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function (prices) {
+  // [7,1,5,3,6,4]
+  if (prices.length === 0) return 0
+
+  // 最低买点
+  let min = prices[0] // 7
+  // 最大收入
+  let max = 0
+
+  for (let p of prices) {
+    min = Math.min(min, p) //7,1,1,1,
+    max = Math.max(max, p - min) //0 1,4,5,
+  }
+
+  return max
+}
+// @lc code=end
