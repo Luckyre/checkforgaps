328524ef32abb6cf14358711d69aa8ed75c29b44
lucklyrs <1305029103@qq.com>
2023-08-07 15:24:56 +0800

    feat: hasCycle

---
+++ base/letcode/141.环形链表.js
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=141 lang=javascript
+ *
+ * [141] 环形链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function (head) {
+  // [3,2,0,-4]
+  // 通过双指针，快慢指针在终点前相遇时，为环形
+  let slow = head, //3
+    fast = head //3
+  while (slow !== null && fast !== null) {
+    slow = slow.next //走一步 //
+    fast = fast.next.next //走两步
+    if (slow === fast) {
+      return true
+    }
+  }
+  return false
+}
+// @lc code=end
