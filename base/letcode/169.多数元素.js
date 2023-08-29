57a45cf22c304a0cc36148857fa83b696972c97f
lucklyrs <1305029103@qq.com>
2023-08-29 10:41:43 +0800

    fix: majorityElement

--- base/letcode/169.多数元素.js
+++ base/letcode/169.多数元素.js
@@ -11,10 +11,11 @@
  */
 var majorityElement = function (nums) {
   //排序数组，若一个数字中的数字出现频率大于n>2, 则在数组中 nums.lneght>2的位置就是这个数
-  nums.sort((a, b) => {
-    // [3,2,3]
-    a - b
-  })
+  nums.sort(
+    (a, b) =>
+      // [3,2,3]
+      a - b
+  )
   //[2,3,3]
   return nums[Math.floor(nums.length / 2)]
 }
