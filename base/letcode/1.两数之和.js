be399bfb89ec552e2709f9895c4ae90325921e87
lucklyrs <1305029103@qq.com>
2024-05-22 17:01:20 +0800

    fix: twoSum

--- base/letcode/1.两数之和.js
+++ base/letcode/1.两数之和.js
@@ -12,23 +12,17 @@
  */
 
 var twoSum = function (nums, target) {
-  // 排序
-  nums.sort((a, b) => a - b)
-
-  let start = 0,
-    end = nums.length - 1
-  //循环对比 移动指针
-  while (start < end) {
-    let sum = nums[start] + nums[end]
-    if (sum < target) {
-      start++
-    } else if (sum > target) {
-      end--
-    } else if (sum === target) {
-      return [nums[start], nums[end]]
+  // 维护 val -> index 的映射
+  var valToIndex = new Map()
+  for (var i = 0; i < nums.length; i++) {
+    // 查表，看看是否有能和 nums[i] 凑出 target 的元素
+    var need = target - nums[i]
+    if (valToIndex.has(need)) {
+      return [valToIndex.get(need), i]
     }
+    // 存入 val -> index 的映射
+    valToIndex.set(nums[i], i)
   }
-  return []
+  return null
 }
-
 // @lc code=end
