461c555071a4ca25131f61691b71474d5eeea18a
lucklyrs <1305029103@qq.com>
2024-08-20 16:42:38 +0800

    feat: 257二叉树的所有路径-深度优先搜索

--- letcode/257.二叉树的所有路径.js
+++ letcode/257.二叉树的所有路径.js
@@ -14,33 +14,33 @@
  * Testcase Example:  '[1,2,3,null,5]'
  *
  * 给你一个二叉树的根节点 root ，按 任意顺序 ，返回所有从根节点到叶子节点的路径。
- * 
+ *
  * 叶子节点 是指没有子节点的节点。
- * 
- * 
+ *
+ *
  * 示例 1：
- * 
- * 
+ *
+ *
  * 输入：root = [1,2,3,null,5]
  * 输出：["1->2->5","1->3"]
- * 
- * 
+ *
+ *
  * 示例 2：
- * 
- * 
+ *
+ *
  * 输入：root = [1]
  * 输出：["1"]
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
  * 提示：
- * 
- * 
+ *
+ *
  * 树中节点的数目在范围 [1, 100] 内
  * -100 <= Node.val <= 100
- * 
- * 
+ *
+ *
  */
 
 // @lc code=start
@@ -56,8 +56,31 @@
  * @param {TreeNode} root
  * @return {string[]}
  */
-var binaryTreePaths = function(root) {
+var binaryTreePaths = function (root) {
+  // 输入：root = [1,2,3,null,5]
+  //  输出：["1->2->5","1->3"]
+  //深度优先搜索
+  const paths = []
+  const construct_paths = (root, path) => {
+    //
+    if (root) {
+      path += root.val.toString() // 1 //1->2  //1->2->5   1->3
 
-};
+      if (!root.left && !root.right) {
+        //一直遍历到叶子节点
+        //
+
+        paths.push(path)
+      } else {
+        path += '->' //1-> //1->2->5  //1->3
+
+        construct_paths(root.left, path)
+        construct_paths(root.right, path)
+      }
+    }
+  }
+
+  construct_paths(root, '')
+  return paths
+}
 // @lc code=end
-
