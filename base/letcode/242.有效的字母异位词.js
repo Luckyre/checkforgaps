4f4c29f3666e9cba20a9688406a50b0cfcb9bf24
lucklyrs <1305029103@qq.com>
2024-08-20 14:57:37 +0800

    feat: 242有效的字母异位词

---
+++ base/letcode/242.有效的字母异位词.js
@@ -0,0 +1,65 @@
+/*
+ * @lc app=leetcode.cn id=242 lang=javascript
+ *
+ * [242] 有效的字母异位词
+ *
+ * https://leetcode.cn/problems/valid-anagram/description/
+ *
+ * algorithms
+ * Easy (66.79%)
+ * Likes:    942
+ * Dislikes: 0
+ * Total Accepted:    827.1K
+ * Total Submissions: 1.2M
+ * Testcase Example:  '"anagram"\n"nagaram"'
+ *
+ * 给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
+ *
+ * 注意：若 s 和 t 中每个字符出现的次数都相同，则称 s 和 t 互为字母异位词。
+ *
+ *
+ *
+ * 示例 1:
+ *
+ *
+ * 输入: s = "anagram", t = "nagaram"
+ * 输出: true
+ *
+ *
+ * 示例 2:
+ *
+ *
+ * 输入: s = "rat", t = "car"
+ * 输出: false
+ *
+ *
+ *
+ * 提示:
+ *
+ *
+ * 1
+ * s 和 t 仅包含小写字母
+ *
+ *
+ *
+ *
+ * 进阶: 如果输入字符串包含 unicode 字符怎么办？你能否调整你的解法来应对这种情况？
+ *
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function (s, t) {
+  //若t是s的异位词等价于两个字符串排序后相等，
+
+  // [...s] = s.split('')
+  return (
+    s.length === t.length &&
+    s.split('').sort().join('') === t.split('').sort().join('')
+  )
+}
+// @lc code=end
