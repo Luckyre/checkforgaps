609b8e2964328922bcc0a8651afbba48a1381ba3
lucklyrs <1305029103@qq.com>
2024-08-23 17:38:58 +0800

    feat: 283 移动零JS

---
+++ letcode/283.移动零.js
@@ -0,0 +1,82 @@
+/*
+ * @lc app=leetcode.cn id=283 lang=javascript
+ *
+ * [283] 移动零
+ *
+ * https://leetcode.cn/problems/move-zeroes/description/
+ *
+ * algorithms
+ * Easy (63.64%)
+ * Likes:    2433
+ * Dislikes: 0
+ * Total Accepted:    1.5M
+ * Total Submissions: 2.3M
+ * Testcase Example:  '[0,1,0,3,12]'
+ *
+ * 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+ *
+ * 请注意 ，必须在不复制数组的情况下原地对数组进行操作。
+ *
+ *
+ *
+ * 示例 1:
+ *
+ *
+ * 输入: nums = [0,1,0,3,12]
+ * 输出: [1,3,12,0,0]
+ *
+ *
+ * 示例 2:
+ *
+ *
+ * 输入: nums = [0]
+ * 输出: [0]
+ *
+ *
+ *
+ * 提示:
+ *
+ *
+ *
+ * 1 <= nums.length <= 10^4
+ * -2^31 <= nums[i] <= 2^31 - 1
+ *
+ *
+ *
+ *
+ * 进阶：你能尽量减少完成的操作次数吗？
+ *
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function (nums) {
+  let slow = 0
+  for (let fast = 0; fast < nums.length; ++fast) {
+    if (nums[fast] !== 0) {
+      nums[slow++] = nums[fast] // 遇到非0元素，就把它交换到左边
+      // 交换后slow++
+    }
+  }
+  for (let fast = slow; fast < nums.length; ++fast) {
+    // 把slow后面的元素都置为0
+    nums[fast] = 0
+  }
+}
+
+var moveZeroes = function (nums) {
+  let slow = 0
+  for (let fast = 0; fast < nums.length; fast++) {
+    if (nums[fast] != 0) {
+      //当前元素!=0，就把其交换到左边，等于0的交换到右边
+      let temp = nums[slow]
+      nums[slow] = nums[fast]
+      nums[fast] = temp
+      slow++
+    }
+  }
+}
+// @lc code=end
