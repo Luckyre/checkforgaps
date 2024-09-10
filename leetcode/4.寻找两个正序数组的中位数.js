4c7fc65d9f6970fb214320bd510204339432a4a0
lucklyrs <1305029103@qq.com>
2024-09-10 09:54:58 +0800

    feat: 寻找两个正序数组的中位数

--- leetcode/4.寻找两个正序数组的中位数.js
+++ leetcode/4.寻找两个正序数组的中位数.js
@@ -14,42 +14,42 @@
  * Testcase Example:  '[1,3]\n[2]'
  *
  * 给定两个大小分别为 m 和 n 的正序（从小到大）数组 nums1 和 nums2。请你找出并返回这两个正序数组的 中位数 。
- * 
+ *
  * 算法的时间复杂度应该为 O(log (m+n)) 。
- * 
- * 
- * 
+ *
+ *
+ *
  * 示例 1：
- * 
- * 
+ *
+ *
  * 输入：nums1 = [1,3], nums2 = [2]
  * 输出：2.00000
  * 解释：合并数组 = [1,2,3] ，中位数 2
- * 
- * 
+ *
+ *
  * 示例 2：
- * 
- * 
+ *
+ *
  * 输入：nums1 = [1,2], nums2 = [3,4]
  * 输出：2.50000
  * 解释：合并数组 = [1,2,3,4] ，中位数 (2 + 3) / 2 = 2.5
- * 
- * 
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
+ *
+ *
  * 提示：
- * 
- * 
+ *
+ *
  * nums1.length == m
  * nums2.length == n
  * 0 <= m <= 1000
  * 0 <= n <= 1000
  * 1 <= m + n <= 2000
  * -10^6 <= nums1[i], nums2[i] <= 10^6
- * 
- * 
+ *
+ *
  */
 
 // @lc code=start
@@ -58,8 +58,15 @@
  * @param {number[]} nums2
  * @return {number}
  */
-var findMedianSortedArrays = function(nums1, nums2) {
-
-};
+var findMedianSortedArrays = function (nums1, nums2) {
+  // 数组合并，排序，取中位数 [1,2] [1]
+  const arr = [...nums1, ...nums2]
+  arr.sort((a, b) => a - b)
+  const len = arr.length
+  if (len % 2 === 0) {
+    return (arr[len / 2] + arr[len / 2 - 1]) / 2
+  } else {
+    return arr[Math.floor(len / 2)]
+  }
+}
 // @lc code=end
-
