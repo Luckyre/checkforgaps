cfd7f71ae39c32276f70f76c7a331c3ae33207a3
lucklyrs <1305029103@qq.com>
2024-09-25 11:39:29 +0800

    feat:斐波那契数

--- leetcode/509.斐波那契数.js
+++ leetcode/509.斐波那契数.js
@@ -73,4 +73,15 @@ var fib = function (n) {
     return fib(n - 1) + fib(n - 2) // fib(1)+ fib(0) =1+0 =1
   }
 }
+
+var fib2 = function (n) {
+  //方法二
+  if (n === 0) return 0
+  if (n === 1) return 1
+  let dp = [0, 1]
+  for (let i = 2; i <= n; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2]
+  }
+  return dp[n]
+}
 // @lc code=end
