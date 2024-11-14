a13737ba4848c1c98482aa9d5231ee545b2fd65d
lucklyrs <1305029103@qq.com>
2024-11-14 11:11:23 +0800

    feat: 575.分糖果

--- leetcode/575.分糖果.js
+++ leetcode/575.分糖果.js
@@ -13,12 +13,26 @@
  * @param {number[]} candyType
  * @return {number}
  */
+//  set
 var distributeCandies = function (candyType) {
   // 通过对candyType去重 得到糖果的种类
   // 然后通过比较糖果的种类和n/2的大小，获取最小值
   let set = new Set(candyType)
   let n = candyType.length
   return Math.min(set.size, n / 2)
+}
+
+var distributeCandies = function (candyType) {
+  // 通过排序
+  // 然后通过比较相邻的糖果种类，获取最小值
+  candyType.sort((a, b) => a - b)
+  let count = 1
+  for (let i = 1; i < candyType.length && count < candyType.length / 2; i++) {
+    if (candyType[i] > candyType[i - 1]) {
+      count++
+    }
+  }
+  return count
 }
 // @lc code=end
 
